0de4afe5add62fbd317563862fdd98e6d4737e85
Niels H. Christensen <nhc@mayacs.com>
2016-02-09 19:53:28 +0000

    Included CSparql and instatiated an engine.

--- src/main/java/uk/ac/abdn/foodsafety/Main.java
+++ src/main/java/uk/ac/abdn/foodsafety/Main.java
@@ -2,6 +2,8 @@ package uk.ac.abdn.foodsafety;
 
 import java.time.LocalDate;
 
+import eu.larkc.csparql.core.engine.CsparqlEngineImpl;
+
 /**
  * 
  * @author nhc
@@ -18,8 +20,14 @@ public class Main {
         final WirelessTagClient client = new WirelessTagClient();
         client.getEventRawData(3);
         client.getStatsRaw(
-                3, 
+                3,
                 LocalDate.of(2016, 1, 28), 
                 LocalDate.of(2016, 1, 29));
+        //Create csparql engine instance
+        CsparqlEngineImpl engine = new CsparqlEngineImpl();
+        //Initialize the engine instance
+        //The initialization creates the static engine (SPARQL) and the stream engine (CEP)
+        engine.initialize();
+
     }
 }
