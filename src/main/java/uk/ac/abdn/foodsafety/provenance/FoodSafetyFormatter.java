16762972a0c13c30f528081d294723988df50732
Niels H. Christensen <nhc@mayacs.com>
2016-03-04 10:41:16 +0000

    Store the SPARQL update queries

--- src/main/java/uk/ac/abdn/foodsafety/provenance/FoodSafetyFormatter.java
+++ src/main/java/uk/ac/abdn/foodsafety/provenance/FoodSafetyFormatter.java
@@ -1,5 +1,8 @@
 package uk.ac.abdn.foodsafety.provenance;
 
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
 import java.util.Observable;
 
 import uk.ac.abdn.foodsafety.common.Logging;
@@ -7,10 +10,18 @@ import eu.larkc.csparql.common.RDFTable;
 import eu.larkc.csparql.core.ResultFormatter;
 
 class FoodSafetyFormatter extends ResultFormatter {
+    /** Name picked up from directory - used for logging */
     private final String queryName;
+    
+    private EnumMap<Stage, List<String>> sparqlUpdateQueries =
+            new EnumMap<Stage, List<String>>(Stage.class);
 
     FoodSafetyFormatter(final String queryName) {
         this.queryName = queryName;
+        //Initialize SPARQL update query collections
+        for (Stage s : Stage.values()) {
+            this.sparqlUpdateQueries.put(s, new ArrayList<String>());
+        }
     }
     
     /**
@@ -23,12 +34,16 @@ class FoodSafetyFormatter extends ResultFormatter {
     }
 
     public void addSparql(final String name, final String content) {
-        // TODO Auto-generated method stub
-        
+        this.sparqlUpdateQueries.get(Stage.valueOf(name.toUpperCase())).add(content);
     }
 
     public void setOwl(final String content) {
         // TODO Auto-generated method stub
         
-    }   
+    }
+    
+    private enum Stage {
+      COLDSTART,
+      WARM;
+    };
 }
